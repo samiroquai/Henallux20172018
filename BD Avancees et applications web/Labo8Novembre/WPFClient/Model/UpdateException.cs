813d9780f640e69ea577fefaee8a198b35eed627
Samuel Scholtes <samuel.scholtes@henallux.be>
2017-11-08 09:03:01 +0100

    Ajout du client WPF nécessaire au labo du 25 octobre.

---
+++ BD Avancees et applications web/Labo8Novembre/WPFClient/Model/UpdateException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFClient.Model
+{
+    public class UpdateException:Exception
+    {
+        public UpdateException(string message)
+            :base("Une erreur s'est produite lors de la mise à jour: "+message)
+        {
+
+        }
+    }
+}
